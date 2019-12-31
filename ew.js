1ec2bb164308254f7def5244563058889063d78f
ckm <by.infofun@gmail.com>
2019-12-31 11:06:38 +0900

    ew

--- ew.js
+++ ew.js
@@ -22,3 +22,28 @@ function api_test(url, data, token){
 		}
 	});
 }
+
+function sendSms_test2(url,idt,token) {
+	api_test2(url, {
+		id : idt
+	}, token)
+}
+
+function api_test2(url, data, token){
+	$.ajax({
+		type: "POST",
+		dataType: "json",
+		url: url,
+		contentType: "application/json",
+		data: JSON.stringify(data),
+		headers: {
+			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		},
+		success: function(d) {
+			console.log(d);
+		},
+		error: function(xhr, textStatus) {
+			console.log('err: '+xhr+' :|: '+textStatus);
+		}
+	});
+}
