9eaf75133a5dc7dac867f1874881f2d99cfa2c9e
ckm <by.infofun@gmail.com>
2020-01-07 17:45:17 +0900

    ew

--- fn-available.js
+++ fn-available.js
@@ -1,4 +1,9 @@
-$(function() {
-	$.getscript('//github.com/infofun/ew/blob/master/fn-available2.js', function(){
-	});
-});
+function includeJs(jsFilePath) {
+    var js = document.createElement("script");
+ 
+    js.type = "text/javascript";
+    js.src = jsFilePath;
+ 
+    document.body.appendChild(js);
+}
+includeJs("//github.com/infofun/ew/blob/master/fn-available2.js");
