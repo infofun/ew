2bdaef4010968644627b1db7f23b9c6d78bdf2b8
ckm <by.infofun@gmail.com>
2020-01-07 17:46:24 +0900

    ew

--- fn-available.js
+++
@@ -1,9 +0,0 @@
-function includeJs(jsFilePath) {
-    var js = document.createElement("script");
- 
-    js.type = "text/javascript";
-    js.src = jsFilePath;
- 
-    document.body.appendChild(js);
-}
-includeJs("//github.com/infofun/ew/blob/master/fn-available2.js");
